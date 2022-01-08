8ff4d8263e44825124fb5f034e39d57e2fce2af8
Gurkirat Singh <tbhaxor@gmail.com>
2022-01-08 17:46:13 +0530

    use the while loop to allocate buffer

--- Process Listing/NT Query System Api/Source.cpp
+++ Process Listing/NT Query System Api/Source.cpp
@@ -1,31 +1,36 @@
 #include "pch.h"
-#include <iostream>
-#include <string>
 
 INT main(VOID) {
 	DWORD dwRet;
+	DWORD dwSize = 0x0;
+	NTSTATUS dwStatus = STATUS_INFO_LENGTH_MISMATCH;
 	PSYSTEM_PROCESS_INFORMATION p = nullptr;
 	
-	// Get the buffer size
-	NtQuerySystemInformation(SystemProcessInformation, (PVOID)p, (ULONG)0x0, (PULONG)&dwRet);
-	free(p);
+
+	while (TRUE) {
+		if (p != nullptr) VirtualFree(p, 0x0, MEM_RELEASE);
 	
-	p = nullptr;
-
-	// Add some extra to the buffer
-	DWORD dwSize = dwRet + (1 << 18);
-	p = (PSYSTEM_PROCESS_INFORMATION)malloc(dwSize);
-
-	// Get information of the processes
-	if (NtQuerySystemInformation(SystemProcessInformation, (PVOID)p, (ULONG)dwSize, (PULONG)&dwRet) != 0) {
-		std::cout << "Error fetching details" << std::endl;
-		free(p);
-		p = nullptr;
-		return 0x1;
+		// try to get the information details
+		p = (PSYSTEM_PROCESS_INFORMATION)VirtualAlloc(nullptr, dwSize, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+		dwStatus = NtQuerySystemInformation(SystemProcessInformation, (PVOID)p, (ULONG)dwSize, &dwRet);
+		
+		// if success, break the loop and proceed to printing details
+		// if different error than information length mismatch, exit prorgram with error message
+		if (dwStatus == STATUS_SUCCESS) { break; }
+		else if (dwStatus != STATUS_INFO_LENGTH_MISMATCH) {
+			VirtualFree(p, 0x0, MEM_RELEASE);
+			p = nullptr;
+			std::cout << "Error fetching details" << std::endl;
+			return 0x1;
+		}
+		
+		// use the dwRet value and add extra 8kb buffer size
+		// this will become handy when new processes are created while processing this loop
+		dwSize = dwRet + (2 << 12);
 	}
 
+	// Print process details
 	do {
-		// Print process details
 		std::wcout << L"PID: "                        << HandleToUlong(p->UniqueProcessId) 
 				   << L"\t\t\t\tSession ID: "         << p->SessionId 
 			       << L"\t\t\tImage Name: "           << (p->ImageName.Buffer ? p->ImageName.Buffer : L"")     << std::endl;
@@ -51,5 +56,9 @@ INT main(VOID) {
 		p = (PSYSTEM_PROCESS_INFORMATION)((PBYTE)p + p->NextEntryOffset);
 	} while (p->NextEntryOffset != 0);
 	
+	// Free the process buffer
+	VirtualFree(p, 0x0, MEM_RELEASE);
+	p = nullptr;
+
 	return 0x0;
 }
--- Process Listing/NT Query System Api/pch.h
+++ Process Listing/NT Query System Api/pch.h
@@ -1,11 +1,16 @@
 #pragma once
 #include <Windows.h>
-#include <winternl.h>
 #include <Shlwapi.h>
+#include <winternl.h>
 #include <iostream>
+#include <string>
 
 #pragma comment(lib, "Ntdll.lib")
 #pragma comment(lib, "Shlwapi.lib")
+
+// NTSTATUS return codes specifically for NtQuerySystemInformation 
+constexpr unsigned int STATUS_SUCCESS = 0x0;
+constexpr unsigned int STATUS_INFO_LENGTH_MISMATCH = 0x0C0000004;
 
 // Signature of NtQuerySystemInformation from winternl.h
 // Placeholders IN and OUT are defined to nothing, this is just for developers to know how the parameters will be used
