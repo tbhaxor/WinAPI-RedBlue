04b157213ef0353cdee3dda3bde19a6220690b45
Gurkirat Singh <tbhaxor@gmail.com>
2022-01-08 13:52:25 +0530

    renamed NT System Query to NT Query System

--- Process Listing/NT Query System Api/pch.h
+++ Process Listing/NT Query System Api/pch.h
@@ -5,6 +5,7 @@
 #pragma comment(lib, "Ntdll.lib")
 
 // Signature of NtQuerySystemInformation from winternl.h
+// Placeholders IN and OUT are defined to nothing, this is just for developers to know how the parameters will be used
 EXTERN_C NTSTATUS NTAPI NtQuerySystemInformation(
 	IN SYSTEM_INFORMATION_CLASS SystemInformationClass,
 	OUT PVOID               SystemInformation,
