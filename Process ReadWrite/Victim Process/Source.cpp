16009251414c042d929e8f8d2b8e90f4d8789488
Gurkirat Singh <tbhaxor@gmail.com>
2022-01-09 00:11:39 +0530

    updated victim process

--- Process ReadWrite/Victim Process/Source.cpp
+++ Process ReadWrite/Victim Process/Source.cpp
@@ -1,17 +1,30 @@
 #include <Windows.h>
-#include <stdio.h>
+#include <iostream>
 
-INT main(VOID) {
-	char* ch = (char*)malloc(20);
+INT main(DWORD argc, PCHAR argv[]) {
+	if (argc < 2) {
+		std::cerr << "Usage: " << argv[0] << " <text>" << std::endl;
+		return 0x1;
+	}
+	SIZE_T txtLen = strlen(argv[1]);
 
-	// Print process details and base address of the data
-	printf("Process ID: %d\n", GetCurrentProcessId());
-	printf("Enter text: ");
-	scanf_s("%19s", ch, 20);
-	printf("Base Address: 0x%p\n\n", ch);
+	std::cout << "Process ID: " << GetCurrentProcessId() << std::endl;
 	
-	// Print the text before and after tamper
-	printf("Text Before Tamper: %s\n", ch);
+	LPSTR lpBuff = (LPSTR)VirtualAlloc(nullptr, txtLen, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+	memcpy(lpBuff, argv[1], txtLen);
+
+	std::cout << "=====================================\n";
+	std::cout << "       Variables before Tamper       \n";
+	std::cout << "=====================================\n\n";
+	std::cout << "Argument address: 0x" << std::hex << &argv[1] << "\t\t\tArgument Value: " << argv[1] << std::endl;
+	std::cout << "Buffer address:   0x" << std::hex << (PVOID)&lpBuff[0] << "\t\t\tBuffer Value: " << lpBuff << std::endl;
+	std::cout << "Length of Chars: " << std::dec << txtLen << std::endl;
+
 	system("pause");
-	printf("Text After Tamper: %s\n", ch);
+
+	std::cout << "=====================================\n";
+	std::cout << "       Variables after Tamper        \n";
+	std::cout << "=====================================\n\n";
+	std::cout << "Argument address: 0x" << std::hex << &argv[1] << "\t\t\tArgument Value: " << argv[1] << std::endl;
+	std::cout << "Buffer address: 0x" << std::hex << (PVOID)&lpBuff[0] << "\t\t\tBuffer Value: " << lpBuff << std::endl;
 }
