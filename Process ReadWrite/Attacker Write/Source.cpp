86704e8d1c80a7005a61508bc8447d5e2fd56ae7
Gurkirat Singh <tbhaxor@gmail.com>
2022-01-09 00:21:54 +0530

    simpified attacker write

--- Process ReadWrite/Attacker Write/Source.cpp
+++ Process ReadWrite/Attacker Write/Source.cpp
@@ -6,28 +6,31 @@ INT main(INT argc, PCHAR argv[]) {
 		return 0x1;
 	}
 
+	// Adding SeDebugPrivilege to the process
+	// If the call is unsuccessfull, exit the program because it will fail during OpenProcess
 	if (!AddSeDebugPrivileges()) {
 		PrintError("AddSeDebugPrivileges()", TRUE);
 	}
 
+	// Handle argument conversions here
 	DWORD dwPID = atoi(argv[1]);
 	LONGLONG addr;
 	SIZE_T dwRet;
 	SIZE_T dwLen = strlen(argv[3]);
 	StrToInt64ExA(argv[2], STIF_SUPPORT_HEX, &addr);
 	
-	CHAR buffer[100];
-	memset(buffer, 0x0, 100);
-	sprintf(buffer, "%s", argv[2]);
-
+	// Open the process handle with VM_WRITE and VM_OPERATION permissions
 	HANDLE hProc = OpenProcess(PROCESS_VM_WRITE | PROCESS_VM_OPERATION, FALSE, dwPID);
 	if (hProc == INVALID_HANDLE_VALUE) {
 		PrintError("OpenProcess()", TRUE);
 	}
 
-	if (!WriteProcessMemory(hProc, (LPVOID)addr, (LPCVOID)buffer, dwLen, &dwRet)) {
-		PrintError("WriteProcessMemory()", TRUE);
+	// Finally write process memory
+	if (!WriteProcessMemory(hProc, (LPVOID)addr, (LPCVOID)argv[3], dwLen, &dwRet)) {
+		PrintError("WriteProcessMemory()", FALSE);
 	} else {
-		printf("Successfully tampered process memory\n");
+		std::cout << "Successfully tampered process memory\n";
 	}
+
+	return 0x0;
 }
