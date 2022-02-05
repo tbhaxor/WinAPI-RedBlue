06020621436b13a00eacb51c4915b3861ebcd6f3
Gurkirat Singh <tbhaxor@gmail.com>
2022-02-05 18:20:59 +0530

    use dynamic allocation

--- Token Dump/Token Dump/TokenSandBoxInert.h
+++ Token Dump/Token Dump/TokenSandBoxInert.h
@@ -11,12 +11,16 @@ VOID PrintTokenSandboxInert(HANDLE hTok) {
 	DWORD dwTokLen = GetTokenInfoLength(hTok, TokenSandBoxInert);
 	DWORD dwRetLen;
 	
-	DWORD dwSandBoxInert;
-	if (!GetTokenInformation(hTok, TokenSandBoxInert, &dwSandBoxInert, dwTokLen, &dwRetLen)) {
+	LPDWORD lpSandBoxInert = (LPDWORD)VirtualAlloc(nullptr, dwTokLen, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+
+	if (!GetTokenInformation(hTok, TokenSandBoxInert, lpSandBoxInert, dwTokLen, &dwRetLen)) {
 		PrintError(L"GetTokenInformation()", FALSE);
-	} else if (dwSandBoxInert) {
+	} else if (*lpSandBoxInert) {
 		std::wcout << L"[+] Sandbox Inert: Exists in Token\n";
 	} else {
 		std::wcout << L"[+] Sandbox Inert: Does not exists in Token\n";
 	}
+
+	VirtualFree(lpSandBoxInert, 0x0, MEM_RELEASE);
+	lpSandBoxInert = nullptr;
 }
--- Token Dump/Token Dump/TokenSessionId.h
+++ Token Dump/Token Dump/TokenSessionId.h
@@ -1,5 +1,6 @@
 #pragma once
 
+
 #include "Utils.h"
 #include "ErrorHandling.h"
 
@@ -11,10 +12,14 @@ VOID PrintTokenSessionId(HANDLE hTok) {
 	DWORD dwTokLen = GetTokenInfoLength(hTok, TokenSessionId);
 	DWORD dwRetLen;
 	
-	DWORD dwSessionId;
-	if (!GetTokenInformation(hTok, TokenSessionId, &dwSessionId, dwTokLen, &dwRetLen)) {
+	LPDWORD lpSessionId = (LPDWORD)VirtualAlloc(nullptr, dwTokLen, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+	
+	if (!GetTokenInformation(hTok, TokenSessionId, lpSessionId, dwTokLen, &dwRetLen)) {
 		PrintError(L"GetTokenInformation()", FALSE);
 	} else {
-		std::wcout << L"[+] Logon Session ID: " << dwSessionId << std::endl;
+		std::wcout << L"[+] Logon Session ID: " << *lpSessionId << std::endl;
 	}
+
+	VirtualFree(lpSessionId, 0x0, MEM_RELEASE);
+	lpSessionId = nullptr;
 }
