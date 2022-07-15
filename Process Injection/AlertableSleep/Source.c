513e319bade3f351ca34236bc36b588066e0a888
Gurkirat Singh <tbhaxor@gmail.com>
2022-07-16 00:53:40 +0530

    improve (alertablesleep): add comments to the functions and some of the body

--- Process Injection/AlertableSleep/Source.c
+++ Process Injection/AlertableSleep/Source.c
@@ -2,9 +2,13 @@
 #include <stdio.h>
 #include <tchar.h>
 
+/// <summary>
+/// Simple thread function to call the sleepex
+/// </summary>
 VOID HelloThread(VOID) {
 	_tprintf(_T("[%d] Thread is going to sleep for 5000 millisecs\n"), GetCurrentThreadId());
 
+	// SleepEx will either be completed when a single APC is executed or it gets timed out
 	if (SleepEx(5000, TRUE) != WAIT_IO_COMPLETION) {
 		_tprintf(_T("[%d] Timed outs\n"), GetLastError());
 	}
@@ -13,11 +17,17 @@ VOID HelloThread(VOID) {
 
 }
 
+/// <summary>
+/// Callback function that will be run from the thread apc queue
+/// </summary>
+/// <param name="dThread">Parameter to hold thread of the APC</param>
 VOID Callback(ULONG_PTR dThread) {
 	_tprintf(_T("[%d APC] Callback invoked!\n"), (DWORD)dThread);
 }
 
 INT _tmain(INT argc, TCHAR argv[]) {
+
+	// create a thread with HelloThread routine
 	DWORD dThreadId;
 	HANDLE hThread = CreateThread(NULL, 0x0, (LPTHREAD_START_ROUTINE)HelloThread, NULL, 0x0, &dThreadId);
 	if (hThread == NULL) {
@@ -29,12 +39,15 @@ INT _tmain(INT argc, TCHAR argv[]) {
 
 	Sleep(4999); // increase time (same as sleep of the thread) and the thread sleepex will timeout
 
-	if (!QueueUserAPC(Callback, hThread, (ULONG_PTR)dThreadId)) {
+	// Add the callback apc to the hThread
+	if (!QueueUserAPC((PAPCFUNC)Callback, hThread, (ULONG_PTR)dThreadId)) {
 		_tprintf(_T("[%d] APC not called\n"), GetLastError());
 		CloseHandle(hThread);
 		return 0x1;
 	}
 
+
+	// Free resources and exit
 	CloseHandle(hThread);
 	hThread = NULL;
 
